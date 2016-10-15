c933d57b87bc68c0c0f064df3f33d9c65fdacc43
HP <hp@HPs-Mac-mini.local>
2016-10-15 23:28:06 +0700

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by HP on 10/15/16.
+//  Copyright © 2016 bokortechnology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by HP on 10/15/16.
+//  Copyright © 2016 bokortechnology. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
